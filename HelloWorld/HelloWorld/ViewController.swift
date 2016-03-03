2000f9a9e498456dcfce685caab30ef451359ed3
正俊 <wakkiy+github@gmail.com>
2016-03-03 09:03:47 +0900

    コンソールに表示

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //画面が表示された時に実行される
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("コンソールテスト");
     }
 
+    //メモリ管理の警告時に実行される
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
