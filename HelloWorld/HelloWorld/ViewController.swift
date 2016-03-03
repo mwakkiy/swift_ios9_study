0505f83d13f68d575baa2b955ca4d0714b19f024
正俊 <wakkiy+github@gmail.com>
2016-03-03 09:18:56 +0900

    ボタンアクションの追加

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //ラベルを使えるように定義
+    @IBOutlet weak var label01: UILabel!
+    //ボタンを押した時発動
+    @IBAction func submit(sender: AnyObject) {
+        print("ボタンを押した");
+        
+        //ラベルのテキスト変更
+        label01.text = "送信";
+    }
+    
+    
     //画面が表示された時に実行される
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //コンソール表示
         print("コンソールテスト");
     }
 
