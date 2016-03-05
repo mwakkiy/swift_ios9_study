57b3e91591227095934f8be526f0f9fcb72d8d54
正俊 <wakkiy+github@gmail.com>
2016-03-05 18:52:11 +0900

    猫の年齢を出力する処理を追加 @todo：入力チェックがまだ未実装

--- CatYear/CatYear/ViewController.swift
+++ CatYear/CatYear/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 猫の入力フォーム
+    @IBOutlet weak var ageInput: UITextField!
+//    //送信ボタン
+//    @IBOutlet weak var submit: UIButton!
+    //変換後のテキスト出力場所
+    @IBOutlet weak var outputText: UILabel!
+    //送信
+    @IBAction func submit(sender: AnyObject) {
+        //猫の年齢
+        let catAge = Int(ageInput.text!)!;
+        let humanAge = catAge * 7;
+        outputText.text = "猫の\(catAge)歳は人間で言うと \(humanAge)歳です。";
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +34,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
