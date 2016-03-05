691ac2b126ae174551f54c1d061e830069c5a417
正俊 <wakkiy+github@gmail.com>
2016-03-05 17:33:06 +0900

    猫の年齢プロジェクト

---
+++ CatYear/CatYear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatYear
+//
+//  Created by masa on 2016/03/05.
+//  Copyright © 2016年 MasatoshiWakizaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
